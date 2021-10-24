8f923797c378dd174087a627f92fed90589102a8
DenisDorogov <denisdorogov@yandex.ru>
2021-10-24 20:02:32 +0500

    Made validation input data & added start from 2

--- prime.js
+++ prime.js
@@ -7,15 +7,46 @@ function output(n) {
     console.log(colors[listOfColors[count % 3]](n));
 }
 
+function validation() {
+    nFrom = Number(from);
+    nTo = Number(to);
+    if (!(Number.isInteger(nFrom) && Number.isInteger(nTo))) {
+        console.log(colors.red("Enter integer number"));
+        return; 
+    };
+    if (nTo < 3) {
+        console.log(colors.red("Enter number more"));
+        return;
+    };
+    if (nFrom > nTo) {
+        console.log(colors.red("Second number must be more"));
+        return;
+    };
+    if (nFrom < 2) nFrom = 2;
+    engine(nFrom, nTo);
+};
+ 
 
-for (let i = from; i <= to; i++) {
-    for (let j = i - 1; j > 1; j--) {
-        if (i % j == 0) {
-            break;
-        };
-        if (j == 2) {
+function engine(from, to) {
+    for (let i = from; i <= to; i++) {
+        if (i == 2) {
             output(i);
             count++;
         };
+        for (let j = i-1; j > 1; j--) {
+            // console.log('i = ', i, 'j = ', j);
+            if (i % j == 0) {
+                // console.log('i % j', i % j == 0);
+                j = 0;
+            };
+            if (j == 2) {
+                output(i);
+                count++;
+            };
+        };
     };
-};
+;}
+
+validation();
+
+
