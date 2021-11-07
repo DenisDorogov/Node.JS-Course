b0bddb548b52e8974c4e1c0fe457e77ec51b675e
DenisDorogov <denisdorogov@yandex.ru>
2021-11-07 21:58:09 +0500

    Сделал вывод каталога. При клике на папку её содержимое выводится пока ниже. Не могу понять как очитсять окно. Более глубокое проваливание пока не работает.

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 const fs =  require('fs');
-const yargs = require('yargs');
-const readline = require('readline');
-//const colors = require('colors');
-//const [from, to] = process.argv.slice(2);
+const path = require('path');
+const http = require('http');
+const url = require('url');
+let dirName = './';
+let list = fs.readdirSync(dirName);
+
+const executionDir = process.cwd();
+const isFile = (fileName) => fs.lstatSync(fileName).isFile();
+
+function outputDir(res, subDir) {
+    console.log('subDir: ', subDir);
+    if (subDir) {
+        dirName = dirName + subDir;
+        console.log('dirName + subDir: ', dirName);
+    };
+    list = fs.readdirSync(dirName);
+    console.log('dirName: ', dirName);
+    console.log('list: ', list);
+    list.forEach(el => {
+        res.write('<a href="/' + el + '">' + el + '</a><br>');
+    });
+};
+
+function choiceFile(res, fileName) {
+            console.log('fileName: ', fileName);
+            if (isFile(fileName)) {
+                // const fullPath = path.join(executionDir, fileName);
+                // fs.readFile(fullPath, 'utf-8', (err, data) => {
+                // if (err) console.log(err);
+                // else console.log(data);
+                // dataString = data;
+                console.log('Open file: ', fileName);
+
+            } else {
+               outputDir(res, fileName);
+            };
+        };
+
+
+
+
+const server = http.createServer((req, res) => {
+    // console.log('req.url: ', req.url);
+    outputDir(res);
+    if (req.url !== '/favicon.ico') {
+        list.forEach(el => {
+            if (req.url === '/' + el) {
+                choiceFile(res, el);
+                res.end();
+            };
+        });
+       
+        res.end();
+    };
+});
+
+server.listen(5555);
 
