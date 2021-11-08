557fd64e19b8470e4f55d368d066469b93fd6ed3
DenisDorogov <denisdorogov@yandex.ru>
2021-11-08 21:44:48 +0500

    Поправил возможность следовать по папкам

--- index.js
+++ index.js
@@ -1,58 +1,65 @@
 const fs =  require('fs');
-const path = require('path');
+// const path = require('path');
 const http = require('http');
 const url = require('url');
-let dirName = './';
-let list = fs.readdirSync(dirName);
+let currentDir = '.';
+let list = fs.readdirSync(currentDir);
 
-const executionDir = process.cwd();
+// const executionDir = process.cwd();
 const isFile = (fileName) => fs.lstatSync(fileName).isFile();
 
-function outputDir(res, subDir) {
-    console.log('subDir: ', subDir);
-    if (subDir) {
-        dirName = dirName + subDir;
-        console.log('dirName + subDir: ', dirName);
-    };
-    list = fs.readdirSync(dirName);
-    console.log('dirName: ', dirName);
+function outputDir(res, path) {
+    currentDir = path + '/';
+    list = fs.readdirSync(path);
     console.log('list: ', list);
     list.forEach(el => {
-        res.write('<a href="/' + el + '">' + el + '</a><br>');
+        if (isFile(currentDir + el)) res.write('<a href="' + currentDir + el + '">' + el + '</a><br>');
+        else res.write('<a href="' + currentDir + el + '">' + el + '/' + '</a><br>');
     });
 };
 
 function choiceFile(res, fileName) {
-            console.log('fileName: ', fileName);
-            if (isFile(fileName)) {
+            let pathFile = currentDir + fileName;
+            console.log('pathFile: ', pathFile);
+            if (isFile(pathFile)) {
                 // const fullPath = path.join(executionDir, fileName);
                 // fs.readFile(fullPath, 'utf-8', (err, data) => {
                 // if (err) console.log(err);
                 // else console.log(data);
                 // dataString = data;
-                console.log('Open file: ', fileName);
+                console.log('Open file: ', pathFile);
 
             } else {
-               outputDir(res, fileName);
+                
+               outputDir(res, pathFile);
             };
         };
 
-
+function getLastItemURN(string) {
+    return string.slice(string.lastIndexOf('/') + 1);
+};
 
 
 const server = http.createServer((req, res) => {
-    // console.log('req.url: ', req.url);
-    outputDir(res);
-    if (req.url !== '/favicon.ico') {
-        list.forEach(el => {
-            if (req.url === '/' + el) {
-                choiceFile(res, el);
-                res.end();
-            };
-        });
-       
-        res.end();
-    };
+    console.log('req.url: ', req.url);
+    switch (req.url) {
+        case  '/favicon.ico': 
+            break;
+        case '/' : 
+            outputDir(res, '.');
+            break;
+        default : 
+            list.forEach(el => {
+                let url = getLastItemURN(req.url);
+                // console.log('url: ', url);
+                if (url === el) {
+                    console.log('el: ', el);
+                    choiceFile(res, el);
+                    res.end();
+                };
+            });
+    } 
+    res.end()
 });
 
 server.listen(5555);
