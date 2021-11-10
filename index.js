d8dad1ff3f8ad5028bc415f3b5e4266b4f406038
DenisDorogov <denisdorogov@yandex.ru>
2021-11-10 19:12:06 +0500

    Gettin starting on lesson 6

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const colors = require('colors');
-const [from, to] = process.argv.slice(2);
-console.log(colors.red("Hello JS from "), colors.green(from), 'to', colors.blue(to) );
+const socket = require('socket.io');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+const server = http
+    .createServer(((req, res) => {
+        const indexPath = path.join(__dirname, 'index.html');
+        const readStream = fs.createReadStream(indexPath);
+
+        readStream.pipe(res);
+    }));
+
+const io = socket(server);
+io.on('connection', client => {
+    console.log(client.id);
+    // console.log('new connection');
+    client.on('client-msg', data => {
+        // console.log(data);
+        // const payload = {
+        //     message: data.message.split('').reverse().join(''),
+        // };
+        const payload = {
+            message: data.message,
+        };
+
+        client.broadcast.emit('server-msg', payload);
+        client.emit('server-msg', payload);
+    });
+});
+
+server.listen(5555);
